aa26936b66bc5a6072c90fa61de65d21726db5af
MohammedSIssa <mohammedsissa1999@gmail.com>
2024-12-04 15:27:58 +0200

    Formatted with Prettier

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const dropdowns = document.querySelectorAll(".dropdown");
 const items = document.querySelectorAll(".dropdown > .items");
 const image = document.querySelector(".image-carousel img");
 const container = document.querySelector(".container");
-let selectedMap = "Cache"
-let imagesCount = 6
-let currentImageIdx = 1
-const dotsContainer = document.querySelector('.dots')
+let selectedMap = "Cache";
+let imagesCount = 6;
+let currentImageIdx = 1;
+const dotsContainer = document.querySelector(".dots");
 
 // Basic Toggle Function (Class "hidden")
 function toggle(dropdownID) {
@@ -37,10 +37,10 @@ for (let i = 0; i < dropdownItems.length; i++) {
         } else if (item_type === "map") {
           image.src = dropdownItems[i].value;
           image.alt = "An Image of " + dropdownItems[i].textContent;
-          selectedMap = dropdownItems[i].getAttribute('map')
-          imagesCount = +dropdownItems[i].getAttribute('images')
-          currentImageIdx = 1
-          handleDots()
+          selectedMap = dropdownItems[i].getAttribute("map");
+          imagesCount = +dropdownItems[i].getAttribute("images");
+          currentImageIdx = 1;
+          handleDots();
           // console.log(selectedMap, imagesCount)
         }
       }
@@ -48,70 +48,68 @@ for (let i = 0; i < dropdownItems.length; i++) {
   });
 }
 
-// Handle Carousel 
+// Handle Carousel
 
 // Prev Button
-const prevBtn = document.querySelector('button.back-btn')
+const prevBtn = document.querySelector("button.back-btn");
 
-prevBtn.addEventListener('click', () => {
-  if(currentImageIdx === 1){
-    currentImageIdx = imagesCount + 1
-    currentImageIdx -= 1
-    image.src = `./images/${selectedMap}-${currentImageIdx}.webp`
-    image.alt = `An Image of ${selectedMap}`
-    selectedDot(currentImageIdx)
+prevBtn.addEventListener("click", () => {
+  if (currentImageIdx === 1) {
+    currentImageIdx = imagesCount + 1;
+    currentImageIdx -= 1;
+    image.src = `./images/${selectedMap}-${currentImageIdx}.webp`;
+    image.alt = `An Image of ${selectedMap}`;
+    selectedDot(currentImageIdx);
+  } else {
+    currentImageIdx -= 1;
+    image.src = `./images/${selectedMap}-${currentImageIdx}.webp`;
+    image.alt = `An Image of ${selectedMap}`;
+    selectedDot(currentImageIdx);
   }
-  else {
-    currentImageIdx -= 1
-    image.src = `./images/${selectedMap}-${currentImageIdx}.webp`
-    image.alt = `An Image of ${selectedMap}`
-    selectedDot(currentImageIdx)
-  }
-})
+});
 
 // Next Button
-nextBtn = document.querySelector('button.next-btn')
-nextBtn.addEventListener('click', () => {
-  if(currentImageIdx === imagesCount){
-    currentImageIdx = 0
-    currentImageIdx += 1
-    image.src = `./images/${selectedMap}-${currentImageIdx}.webp`
-    image.alt = `An Image of ${selectedMap}`
-    selectedDot(currentImageIdx)
+nextBtn = document.querySelector("button.next-btn");
+nextBtn.addEventListener("click", () => {
+  if (currentImageIdx === imagesCount) {
+    currentImageIdx = 0;
+    currentImageIdx += 1;
+    image.src = `./images/${selectedMap}-${currentImageIdx}.webp`;
+    image.alt = `An Image of ${selectedMap}`;
+    selectedDot(currentImageIdx);
+  } else {
+    currentImageIdx += 1;
+    image.src = `./images/${selectedMap}-${currentImageIdx}.webp`;
+    image.alt = `An Image of ${selectedMap}`;
+    selectedDot(currentImageIdx);
   }
-  else {
-    currentImageIdx += 1
-    image.src = `./images/${selectedMap}-${currentImageIdx}.webp`
-    image.alt = `An Image of ${selectedMap}`
-    selectedDot(currentImageIdx)
-  }
-})
+});
 
-// Show/hide carousel buttons on hover 
-const carouselBox = document.querySelector('.image-carousel')
-const carouselButtons = document.querySelector('.carousel-buttons')
-carouselBox.addEventListener('mouseenter', () => {
-  document.querySelector('.carousel-buttons').className = 'carousel-buttons'
-})
+// Show/hide carousel buttons on hover
+const carouselBox = document.querySelector(".image-carousel");
+const carouselButtons = document.querySelector(".carousel-buttons");
+carouselBox.addEventListener("mouseenter", () => {
+  document.querySelector(".carousel-buttons").className = "carousel-buttons";
+});
 
-carouselBox.addEventListener('mouseleave', () => {
-  carouselButtons.className = 'carousel-buttons no-opacity'
-})
+carouselBox.addEventListener("mouseleave", () => {
+  carouselButtons.className = "carousel-buttons no-opacity";
+});
 
 // Handle Dots
-function handleDots(){
-  dotsContainer.innerHTML = ''
-  for(let i = 0; i < imagesCount; i++){
-    let dot = document.createElement('div')
-    i === 0 ? dot.className = 'dot current' : dot.className = 'dot'
-    dotsContainer.appendChild(dot)
+function handleDots() {
+  dotsContainer.innerHTML = "";
+  for (let i = 0; i < imagesCount; i++) {
+    let dot = document.createElement("div");
+    i === 0 ? (dot.className = "dot current") : (dot.className = "dot");
+    dotsContainer.appendChild(dot);
   }
 }
 
-function selectedDot(idx){
-  const dots = document.querySelectorAll('.dots > .dot')
-  dots.forEach(dot => {
-    dot.className = 'dot'
-  })
-  dots[idx - 1].className = 'dot current'
+function selectedDot(idx) {
+  const dots = document.querySelectorAll(".dots > .dot");
+  dots.forEach((dot) => {
+    dot.className = "dot";
+  });
+  dots[idx - 1].className = "dot current";
 }
